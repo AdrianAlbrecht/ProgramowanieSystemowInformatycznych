97fd9911f9174c86f7cc68b29c2ee97c3d045bed
Janek <xivoks1@gmail.com>
2021-12-03 12:47:19 +0100

    creating serializers #2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+from vaccinationRegistrySite.vaccinationProject.serializers import FacilitySerializer
+
+
+def test_FacilitySerializer():
+    val_dict = {'name': 'test_name', 'country':'test_country','city': 'test_city', 'street':'test_street','contact_phone': '123123123'}
+    serializer = FacilitySerializer(data=val_dict)
+    if serializer.is_valid():
+        print(serializer.data)
+        return serializer.data # assertion test here....
+    return serializer.errors
+
+test_FacilitySerializer()
