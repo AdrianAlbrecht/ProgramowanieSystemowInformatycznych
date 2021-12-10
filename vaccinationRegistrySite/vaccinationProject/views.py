9cc258e4721c13b67d7b503715311c212d3d9acf
Janek <xivoks1@gmail.com>
2021-12-10 12:06:59 +0100

    test serializer

--- vaccinationRegistrySite/vaccinationProject/views.py
+++ vaccinationRegistrySite/vaccinationProject/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from vaccinationRegistrySite.vaccinationProject.serializers import FacilitySerializer
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the vaccination page.")
+    val_dict = {'name': 'test_name', 'country': 'test_country', 'city': 'test_city', 'street': 'test_street',
+                'contact_phone': '123123123'}
+    serializer = FacilitySerializer(data=val_dict)
+    if serializer.is_valid():
+        print(serializer.data)
+        return serializer.data  # assertion test here....
+    return serializer.errors
+    # return HttpResponse("Hello, world. You're at the vaccination page.")
