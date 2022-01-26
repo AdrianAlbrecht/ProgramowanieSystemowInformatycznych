5021cc201a00ea5e1371aee62c7192e9068c0534
AdrianAlbrecht <155376@student.uwm.edu.pl>
2022-01-26 21:10:22 +0100

    test fix 2

--- vaccinationRegistrySite/vaccinationProject/tests/test_models.py
+++ vaccinationRegistrySite/vaccinationProject/tests/test_models.py
@@ -1,13 +1,11 @@
-
-
 from django.test import TestCase
 
-from vaccinationRegistrySite.vaccinationProject.models import Facility, UserDetails, Vaccine, Visit
-
+from vaccinationProject.models import Facility, UserDetails, Vaccine, Visit
+from django.contrib.auth.models import User as AuthUser
 
 class TestModels(TestCase):
     def setUp(self):
-        self.user1=UserDetails.object.create(
+        self.user1=UserDetails.objects.create(
             firstname = 'Karol',
             lastname = 'Wallen',
             pesel = '62091692878',
@@ -21,27 +19,28 @@ class TestModels(TestCase):
             phone_number = '606505986',
             gender = 'male',
             is_vaccinated = True,
-        ),
+        )
         self.vaccine1 = Vaccine.objects.create(
             name='Pfizer',
             manufacturer='PfizerCorporation',
             expiration_date='1999-11-21'
-        ),
+        )
         self.facility1 = Facility.objects.create(
             name = 'Placówka Warszawka',
             country = 'Poland',
             city = 'Warszawa',
             street = 'Gdańska',
             contact_phone = '504560982',
-        ),
+        )
         self.visit1 = Visit.objects.create(
             visit_date = '2022-01-01',
             visit_time = '11:10:00',
-            id_patient = 1,
-            id_facility = 1,
-            id_vaccine = 1,
+            id_patient = None,
+            id_facility = self.facility1,
+            id_vaccine = self.vaccine1,
             took_place=0
         )
+        
     def test_userDetails(self):
         self.assertEquals(self.user1.firstname,'Karol')
     
@@ -49,7 +48,7 @@ class TestModels(TestCase):
         self.assertEquals(self.vaccine1.name,'Pfizer')
     
     def test_facitity(self):
-        self.assertEquals(self.vaccine1.name,'Placówka Warszawka')
+        self.assertEquals(self.facility1.city,'Warszawa')
     
     def test_visit(self):
         self.assertEquals(self.visit1.visit_date,'2022-01-01')
